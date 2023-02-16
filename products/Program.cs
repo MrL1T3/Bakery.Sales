97a042e8253a8852e461ff710be3ef063cddd503
Emil Anton <emilbstn@gmail.com>
2023-02-16 08:46:52 -0800

    add final functionality to Program.cs and enable it in Bakery.Sales.csproj

--- products/Program.cs
+++ products/Program.cs
@@ -7,7 +7,14 @@ namespace Bakery.Sales
   {
     public static void Main()
     {
-      Console.WriteLine("Would you like to order bread or pastries?")
+      Console.WriteLine("Would you like to order bread or pastries?");
+      string inputProduct = Console.ReadLine();
+      Console.WriteLine("And how many of that would you like?");
+      int inputAmount = Int32.Parse(Console.ReadLine());
+      BakeOrder Fresh = new BakeOrder(inputProduct, inputAmount);
+      int cost = Fresh.OrderCost;
+      Console.WriteLine("Your order will cost " + cost + " dollars.");
+      Main();
     }
   }
 }
