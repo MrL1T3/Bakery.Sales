f8987a7f70e6f90c68ce5aff340f1613675ea055
Emil Anton <emilbstn@gmail.com>
2023-02-27 12:09:44 -0800

    refactoring BakeOrder class to BreadOrder to calculate bread costs alone

--- products/Models/Products.cs
+++ products/Models/Products.cs
@@ -8,46 +8,35 @@ namespace Bakery.Sales.Models
   {
     public int OrderCost {get; set;}
     public string FoodType {get; set;}
-    public BakeOrder (string FoodChoice, int FoodNum)
+    public BreadOrder ( int FoodNum)
     {
-      FoodType = FoodChoice;
-      int FreeNum = 0;
-      int UnitCost = 0;
       if (FoodType == "bread")
       {
-        FreeNum = 3;
-        UnitCost = 5;
-      }
-      else if (FoodType == "pastries")
+        if ((FoodNum) % 3 == 0)
       {
-        FreeNum = 4;
-        UnitCost = 2;
+        int FoodNumTrue = (FoodNum-(FoodNum/3));
+        OrderCost = FoodNumTrue * 5;
       }
-      else
-      {
-        Console.WriteLine("Our shop sadly doesn't sell that right now, did you want bread or pastries?");
-      }
-
-      if ((FoodNum) % FreeNum == 0)
-      {
-        int FoodNumTrue = (FoodNum-(FoodNum/FreeNum));
-        OrderCost = FoodNumTrue * UnitCost;
-      }
-      else if (((FoodNum) % FreeNum != 0)&&(FoodNum > FreeNum))
+      else if (((FoodNum) % 3 != 0)&&(FoodNum > 3))
       {
         int FoodExtra = 0;
-        while (FoodNum % FreeNum != 0)
+        while (FoodNum % 3 != 0)
         {
           FoodExtra++;
           FoodNum--;
         }
-        int FoodNumTrue = (FoodNum-(FoodNum/FreeNum));
-        OrderCost = (FoodNumTrue + FoodExtra) * UnitCost;
+        int FoodNumTrue = (FoodNum-(FoodNum/3));
+        OrderCost = (FoodNumTrue + FoodExtra) * 5;
       }
       else 
       {
-        OrderCost = FoodNum * UnitCost;
+        OrderCost = FoodNum * 5;
+      }
+      }
+      else
+      {
+        Console.WriteLine("Our shop sadly doesn't sell that right now, did you want bread and/or pastries?");
       }
     }
-  } 
+  }
 }
