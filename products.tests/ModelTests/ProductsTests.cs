ad7150b177a2ed0a4c81f652a92e04b8b1233ef0
Emil Anton <emilbstn@gmail.com>
2023-02-27 19:41:50 -0800

    change tests to accomodate future cost calculation method

--- products.tests/ModelTests/ProductsTests.cs
+++ products.tests/ModelTests/ProductsTests.cs
@@ -10,24 +10,28 @@ namespace Products.Tests
     public void IsBreadCost_EqualToFive_True()
     {
       BreadOrder Fresh = new BreadOrder(1);
+      Fresh.BreadCost(1);
       Assert.AreEqual(5, Fresh.OrderCost);
     }
     [TestMethod]
     public void IsBreadCost_EqualToTen_True()
     {
       BreadOrder Fresh = new BreadOrder(3);
+      Fresh.BreadCost(3);
       Assert.AreEqual(10, Fresh.OrderCost);
     }
     [TestMethod]
     public void IsBreadCost_EqualToTwenty_True()
     {
       BreadOrder Fresh = new BreadOrder(5);
+      Fresh.BreadCost(5);
       Assert.AreEqual(20, Fresh.OrderCost);
     }
     [TestMethod]
     public void IsPastryCost_EqualToSix_True()
     {
       PastryOrder Fresh = new PastryOrder(4);
+      Fresh.PastryCost(4);
       Assert.AreEqual(6, Fresh.OrderCost);
     }
   }
